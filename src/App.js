97ac1312a3d419b1f39178db8f1de93e78b56e10
Kartik Agrawal <kartik.agrawal1_cs18@gla.ac.in>
2021-01-17 16:07:39 +0530

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const b1 = "#3c1361";
+    const text = "Click Me to See Magic"
+    const [bg, setbg] = useState(b1);
+    const [bg1, setbg1] = useState("#6a0dad");
+    const [clr, setclr] = useState("black");
+    const [txt, settxt] = useState(text);
+    const change = () => {
+        const b2 = "#6a0dad";
+        if (bg===b1) {
+            setbg(b2);            
+        }
+        else{
+            setbg(b1);
+        }
+        if (bg1 === b2) {
+            setbg1(b1);
+        }
+        else{
+            setbg1(b2);
+        }
+        if (clr === "black") {
+            setclr("white");
+        }
+        else{
+            setclr("black");
+        }
+        if (txt === text) {
+            settxt("Here is the Magic 😍😻");
+        }
+        else{
+            settxt(text);
+        }
+    }
+    const div1 = {
+        backgroundColor : bg,
+        height : "100vh",
+        display : "flex",
+        justifyContent : "center",
+        alignItems : "center" 
+    }
+
+    const btn = {
+        padding : "20px",
+        outline : "0",
+        border : "0",
+        fontSize : "2em",
+        backgroundColor : bg1,
+        cursor : "pointer",
+        color : clr
+    }
+    return(
+        <>
+            <div style={div1}>
+                <button style = {btn} onClick = {change}>{txt}</button>
+            </div>
+        </>
+    );
+};
 
 export default App;
